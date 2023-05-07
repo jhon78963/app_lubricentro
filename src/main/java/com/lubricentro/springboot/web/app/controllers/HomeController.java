6c5251862de01648341f0f354d4cbd176e4f0f83
bigzero <jhonlivias3@gmail.com>
2023-05-07 11:53:30 -0500

    initial project

---
+++ src/main/java/com/lubricentro/springboot/web/app/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.lubricentro.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("titulo", "Bienvenid@, pagina de inicio");
+		return "home";
+	}
+	
+	@GetMapping("/hola")
+	public String hola(Model model) {
+		model.addAttribute("titulo", "Bienvenid@, pagina de inicio");
+		return "hola";
+	}
+}
